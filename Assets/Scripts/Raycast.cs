c4bf1eaf7b1aa600d0828c8472eaa67e1f963161
Madcrafty <53935032+Madcrafty@users.noreply.github.com>
2021-04-07 22:16:55 +1000

    interactable world space buttons

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Raycast : MonoBehaviour
 {
-    //public TextMeshProUGUI output;
+    public TextMeshProUGUI output;
 
     // Update is called once per frame
     void Update()
@@ -15,7 +16,15 @@ public class Raycast : MonoBehaviour
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo, 500) == true)
         {
-            //output.text = hitInfo.transform.name;
+            output.text = hitInfo.transform.name;
+            if (hitInfo.transform.GetComponent<Button>() != null)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hitInfo.transform.GetComponent<Button>().onClick.Invoke();
+                }
+                
+            }
         }
     }
 }
---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    public GameObject thing;
+    void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(Spawn);
+    }
+    void Spawn()
+    {
+        thing.SetActive(!thing.activeInHierarchy);
+    }
+}
